421ce9b5be79268bf9d82ee99a0f590ea559b6f8
Bebe Jane <bjornthief@gmail.com>
2023-09-14 10:43:07 +0200

    no link to tab

--- pages/vad-vi-gor/[slug].tsx
+++ pages/vad-vi-gor/[slug].tsx
@@ -13,15 +13,6 @@ export type Props = {
   whatType: WhatRecord
 }
 
-function LinkRenderer(props: any) {
-  return (
-    <a href={props.href} target="_blank" rel="noreferrer">
-      {props.children}
-    </a>
-  );
-}
-
-
 export default function WhatWeDo({ whatExamples, whatType }: Props) {
 
   return (
@@ -67,7 +58,7 @@ export default function WhatWeDo({ whatExamples, whatType }: Props) {
                     </li>
                   }
                 </ul>
-                <Markdown className={s.text} components={{ a: LinkRenderer }}>{text}</Markdown>
+                <Markdown className={s.text}>{text}</Markdown>
               </div>
             </Reveal>
 
